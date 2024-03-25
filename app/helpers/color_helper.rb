5eed7e4c9f963d68b7c7a881572a3387274b2179
kenta-crv <okuyamakenta@gmail.com>
2024-03-25 15:41:45 +0900

    first

--- app/helpers/color_helper.rb
+++ app/helpers/color_helper.rb
@@ -6,23 +6,6 @@ module ColorHelper
     private
   
     def set_main_color
-      case "#{controller_path}##{action_name}"
-      when 'top#cocacola'
-        '#F40002'
-      when 'top#dydo'
-        '#F7B106'
-      when 'top#itoen'
-        '#009E42' 
-      when 'top#asahi'
-        '#003591' 
-      when 'top#kirin'
-        '#E40114' 
-      when 'top#otsuka'
-        '#88B9E2' 
-      when 'top#suntory'
-        '#234DB4' 
-      else
-        '#0000FF'
-      end
+        '#004D2B'
     end
   end
