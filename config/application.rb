a387b40adf3476b705b260e2e755aa8cbb2acb31
kenta-crv <okuyamakenta@gmail.com>
2024-05-16 16:46:05 +0900

    mail fix

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-# Dotenv::Railtie.load この行を削除
 
 module Eatery
   class Application < Rails::Application
@@ -21,15 +20,16 @@ module Eatery
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    ActionMailer::Base.delivery_method = :smtp
-    ActionMailer::Base.smtp_settings = {
-      address: Rails.application.credentials.smtp[:address],
-      domain: Rails.application.credentials.smtp[:domain],
-      port: Rails.application.credentials.smtp[:port],
-      user_name: Rails.application.credentials.smtp[:user_name],
-      password: Rails.application.credentials.smtp[:password],
-      authentication: 'plain',
-      enable_starttls_auto: true
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+    address: 'smtp.lolipop.jp',
+    domain: 'factoru.jp',
+    port: 587,
+    user_name: 'payrent@factoru.jp',
+    password: ENV['EMAIL_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
     }
   end
+
 end
